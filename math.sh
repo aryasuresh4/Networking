14077ac5d03bdf4867ad4b53ce264e94e8a1d9d6
aryasuresh4 <117814991+aryasuresh4@users.noreply.github.com>
2023-04-05 12:05:15 +0530

    Add files via upload

---
+++ math.sh
@@ -0,0 +1,11 @@
+echo Enter the number 
+ read a
+ echo Enter the number
+ read b
+ c=`expr $a + $b`
+ echo Addition= $c
+ d=`expr $a - $b`
+ echo subtraction=$b 
+ e=`expr $a \* $b`
+ echo Multipliccation=$e
+
