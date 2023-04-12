67dd0a4072e35cbc595bdb100f2967952c800e18
aryasuresh4 <117814991+aryasuresh4@users.noreply.github.com>
2023-04-12 09:42:55 +0530

    Add files via upload

---
+++ average.sh
@@ -0,0 +1,30 @@
+echo "Name of student:"
+read name
+echo "student registration number:"
+read student registration number
+echo "Enter Marks obtained in DFS: "
+read m1
+echo "Enter marks obtained in OOP: "
+read m2
+echo "Enter marks obtained in OS: "
+read m3
+total=`expr $m1 + $m2 + $m3`
+avg=`expr $total / 3`
+echo "Total: $total"
+echo "Average: $avg"
+
+if [ $avg -ge 90 ]
+then
+echo "Grade = S"
+elif [ $avg -le 90 ] && [ $avg -ge 50 ]
+then
+echo "Grade = A"
+elif  [ $avg -le 80 ] && [ $avg -ge 60 ]
+then
+echo "Grade = B"
+elif [ $avg -le 80 ] && [ $avg -ge 40 ]
+then
+echo "Grade = P"
+else
+echo "Grade = F"
+fi
