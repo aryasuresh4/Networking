7b9a50d91b274a6d103953368e32bb0220be3774
aryasuresh4 <117814991+aryasuresh4@users.noreply.github.com>
2023-06-14 10:43:53 +0530

    Update multitable.sh

--- multitable.sh
+++ multitable.sh
@@ -8,3 +8,19 @@ for ((i = 1; i <= 10; i++)); do
     result=$((number * i))
     echo "$number x $i = $result"
 done
+
+///
+Enter a number:
+4
+Multiplication table for 4:
+4 x 1 = 4
+4 x 2 = 8
+4 x 3 = 12
+4 x 4 = 16
+4 x 5 = 20
+4 x 6 = 24
+4 x 7 = 28
+4 x 8 = 32
+4 x 9 = 36
+4 x 10 = 40
+
